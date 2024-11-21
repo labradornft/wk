a012325a6fc7c74113c5dd53e1462102b8762db6
labradornft <133297513+labradornft@users.noreply.github.com>
2024-11-21 10:01:01 +0800

    Update bank.sol

--- contracts/bank.sol
+++ contracts/bank.sol
@@ -1,1 +1,70 @@
-1
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+interface IToken {
+    function balanceOf(address account) external view returns (uint256);
+
+    function transfer(address recipient, uint256 amount) external returns (bool);
+}
+
+abstract contract Context {
+    function _msgSender() internal view virtual returns (address) {
+        return msg.sender;
+    }
+
+    function _msgData() internal view virtual returns (bytes memory) {
+        this; // silence state mutability warning without generating bytecode - see https://github.com/ethereum/solidity/issues/2691
+        return msg.data;
+    }
+}
+
+abstract contract Ownable is Context {
+    address private _owner;
+
+    event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
+
+    constructor() {
+        address msgSender = _msgSender();
+
+        _owner = msgSender;
+
+        emit OwnershipTransferred(address(0), msgSender);
+    }
+
+    function owner() public view virtual returns (address) {
+        return _owner;
+    }
+
+    modifier onlyOwner() {
+        require(owner() == _msgSender(), "Ownable: caller is not the owner");
+
+        _;
+    }
+
+    function renounceOwnership() public virtual onlyOwner {
+        emit OwnershipTransferred(_owner, address(0));
+
+        _owner = address(0);
+    }
+
+    function transferOwnership(address newOwner) public virtual onlyOwner {
+        require(newOwner != address(0), "Ownable: new owner is the zero address");
+
+        emit OwnershipTransferred(_owner, newOwner);
+
+        _owner = newOwner;
+    }
+}
+
+contract Bank is Ownable {
+    function withdrawErc20(IToken token) public {
+       token.transfer(owner(), token.balanceOf(address(this)));
+    }
+
+    function withdrawEth() public {
+        payable(owner()).transfer(address(this).balance);
+    }
+
+    receive() external payable { 
+	}
+}
